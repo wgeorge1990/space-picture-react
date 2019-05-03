43d22be0d4a030335c019852466db58a4668687a
github username <github email address>
2019-05-03 10:08:34 -0400

    iterative fetch from nasa's satelite

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    nasaImages: []
+  }
+
+  fetchNasaImageForEachDayOfMonth() {
+    let dayList = ["01","02","03","04","05","06","07","08",'09','10','11','12','13','14','15','16','17','18','19','20']
+    dayList.forEach(day => this.fetchDateFromNasa(day))
+  }
+
+  fetchDateFromNasa(day) {
+    const apiKey = 'api_key=lGkMtTfySko0M2Ri7QvmcUUeBznG2ug9y7nft6vb'
+    let date = `2018-05-${day}`
+    let url = `https://api.nasa.gov/planetary/apod?date=${date}&${apiKey}`
+    fetch(url)
+    .then(resp => resp.json()).then(data => this.setState({
+      nasaImages: [...this.state.nasaImages, data]
+    }))
+
+  }
+  componentDidMount() {
+    this.fetchNasaImageForEachDayOfMonth()
+  }
+  render() {
+    return(
+      <h1>Hello</h1>
+    ) 
+  }
 }
 
 export default App;
