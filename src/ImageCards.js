0f0dc9a7a71aff8a64ac771b1be326778ec77b8f
github username <github email address>
2019-05-06 17:25:33 -0400

    added db json and placeheld functions

--- src/ImageCards.js
+++ src/ImageCards.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { Card, Image } from 'semantic-ui-react'
+import { Card, Image, Button } from 'semantic-ui-react'
+
+const saveImageToDbJson = () => {
+    console.log("saveImageToDbJson():" )
+}
+
+const showDescription = () => {
+    console.log('showDescription():')
+}
 
 const card = (image) => {
     return (
@@ -8,8 +16,14 @@ const card = (image) => {
             <Card.Header textAlign="center">
                 {image.title}
             </Card.Header>
-            <p>{image.explanation}</p>
+            {/* <p>{image.explanation}</p> */}
             <Image src={image.hdurl} />
+            <Button  onClick={saveImageToDbJson}>
+                Save Image
+            </Button>
+            <Button  onClick={showDescription}>
+                Show Description
+            </Button>
         </Card.Content>
     </Card> )
 }
